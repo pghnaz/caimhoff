7f8d4c847f420f8e6472bbaee568f2dc288863c4
Reuben L. Lillie <reubenlillie@gmail.com>
2019-07-21 22:20:06 -0500

    Adjust favicon files paths

--- shortcodes/favicon.js
+++ shortcodes/favicon.js
@@ -21,14 +21,14 @@ module.exports = function (eleventyConfig) {
 	 */
 	eleventyConfig.addShortcode('favicon', function () {
 		return `
-			<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
-			<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
-			<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
-			<link rel="manifest" href="/favicons/site.webmanifest">
-			<link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#101820">
-			<link rel="shortcut icon" href="/favicons/favicon.ico">
+			<link rel="apple-touch-icon" sizes="180x180" href="/branding/favicons/apple-touch-icon.png">
+			<link rel="icon" type="image/png" sizes="32x32" href="/branding/favicons/favicon-32x32.png">
+			<link rel="icon" type="image/png" sizes="16x16" href="/branding/favicons/favicon-16x16.png">
+			<link rel="manifest" href="/branding/favicons/site.webmanifest">
+			<link rel="mask-icon" href="/branding/favicons/safari-pinned-tab.svg" color="#101820">
+			<link rel="shortcut icon" href="/branding/favicons/favicon.ico">
 			<meta name="msapplication-TileColor" content="#ffb612">
-			<meta name="msapplication-config" content="/favicons/browserconfig.xml">
+			<meta name="msapplication-config" content="/branding/favicons/browserconfig.xml">
 			<meta name="theme-color" content="#101820">`
 	})
 
